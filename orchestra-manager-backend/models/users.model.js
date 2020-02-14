91385f04943859534d782dc4496ef955d7d64967
Criaric <49854536+Criaric@users.noreply.github.com>
2020-02-13 18:53:51 -0600

    Created User Schema
    Created User Schema
    Installed mongooseUniqueValidator

--- orchestra-manager-backend/models/users.model.js
+++ orchestra-manager-backend/models/users.model.js
@@ -1,1 +1,19 @@
-console.log('hello world from users.model');
+// Test Require
+//console.log('hello world from users.model');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = new Schema({
+    userType:    {type: String, required: true},                 //Admin, Orchestra Manager, Librarian, Member
+    fName:       {type: String, required: true},
+    lName:       {type: String, required: true},
+    password:    {type: String, required: true},                 //Needs to be hashed later
+    email:       {type: String, required: true, unique: true},
+    userName:    {type: String, required: true},
+
+})
+
+Schema.plugin(mongooseUniqueValidator);
+module.exports = mongoose.model('userSchema', userSchema);
