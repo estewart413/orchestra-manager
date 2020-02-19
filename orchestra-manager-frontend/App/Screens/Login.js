76a0f1c5cf61153706c3b152976ed65c9bb5e91b
heidiWesty <heidi.diggs@selu.edu>
2020-02-19 16:45:13 -0600

    made some textboxes that allow user input

--- orchestra-manager-frontend/App/Screens/Login.js
+++ orchestra-manager-frontend/App/Screens/Login.js
@@ -1,40 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Alert, TextInput } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Alert,
+  TextInput
+} from "react-native";
 
-function Separator(){
+function Separator() {
   return <View style={styles.separator} />;
 }
 
 export default function App() {
+  const [value, onChangeText] = React.useState("User Name");
+
   return (
-    
-      <SafeAreaView style={styles.container}>
-       <View >
-         <Text>hello there</Text>
-         <View>
-           <Separator />
-         </View>
-          <Button onPress={() => Alert.alert('button pushed')}
-          title="Log In"
-          color='#3cb371'
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TextInput
+          style={{
+            height: 35,
+            width: 150,
+            borderColor: "black",
+            borderWidth: 1,
+            backgroundColor: "white"
+          }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
         />
+        <Separator />
+        <TextInput
+          style={{
+            height: 35,
+            borderColor: "black",
+            borderWidth: 1,
+            backgroundColor: "white"
+          }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+        <View>
+          <Separator />
         </View>
-      </SafeAreaView>
-      
-    
+        <Button
+          onPress={() => Alert.alert("button pushed")}
+          title="Log In"
+          color="#3cb371"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#008080',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#008080",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   separator: {
-    marginVertical: 15,
-  },
+    marginVertical: 10
+  }
 });
