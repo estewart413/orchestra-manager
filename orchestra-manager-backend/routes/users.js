7267a1571e3b0b140d34adfba2161e32f565889a
estewart413 <dane.stewart@selu.edu>
2020-02-20 07:53:24 -0600

    Establish full User Routing
    Update user.js with  full CRUD routing for users.

--- orchestra-manager-backend/routes/users.js
+++ orchestra-manager-backend/routes/users.js
@@ -7,18 +7,56 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 });
 
+router.route('/:id').get((req, res) => {
+    User.findById(req.params.id)
+        .then(user => res.json(excercise))
+        .catch(err => res.status(400).json('Error' + err))
+});
+
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
+    const userType = req.body.userType;
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+    const password = req.body.password;
+    const email = req.body.email;
+    const userName = req.body.username;
 
     const newUser = new User({
-        username,
-        fullName,
-        passHash,
+        userType,
+        fName,
+        lName,
+        password,
+        email,
+        userName,
     });
 
     newUser.save()
         .then(() => res.json('User saved!'))
         .catch(err => res.status(400).json('Error: ' + err))
+});
+
+router.route('/delete/:id').delete((req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() =>res.json('User Deleted'))
+        .catch(err => res.status(400).json("Error: " + err))
+});
+
+router.route('/edit/:id').put((req, res) => {
+    User.findbyID(req.params.id)
+        .then(user => {
+            if (user.username != req.body.username)
+                user.username = req.body.username;
+            if (user.fName != req.body.fName)
+                user.fName = req.body.fName;
+            if (user.lName != req.body.lName)
+                user.lName = req.body.lName;
+            if (user.password != req.body.password)
+                user.password = req.body.password;
+            if (user.email != req.body.email)
+                user.email = req.body.email;
+            if (user.userType != req.body.userType)
+                user.userType = req.body.userType;
+        });
 });
 
 
