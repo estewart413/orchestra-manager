f0c531d8dfc682311c7613eb1b2a5c973a6259be
Mike Levytskyi <47829986+SkyWalker9955@users.noreply.github.com>
2020-04-06 15:19:05 -0500

    useEffect added

--- orchestra-manager-frontend/App/Screens/debug.js
+++ orchestra-manager-frontend/App/Screens/debug.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 import mongoServer from '../../api/mongoServer';
 import axios from 'axios';
@@ -6,10 +6,23 @@ import axios from 'axios';
 const debug = () => {
     
     const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(mongoServer);
+
+            setInfo(result.info)
+        };
+        fetchData();
+    }, [])
     
     return (
         <View>
-            <FlatList>setInfo</FlatList>
+            {info.map(item => (
+                <FlatList key={item.objectID}>
+                    <Text> href={item.url}>{item.title}</Text>
+                </FlatList>
+            ))}
         </View>
     );
 };
