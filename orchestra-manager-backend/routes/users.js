aaceed1826d7f5a64f19307404aa4dc087d23ff9
Criaric <49854536+Criaric@users.noreply.github.com>
2020-04-09 17:56:59 -0500

    Updated GET and UserSchema
    +Added .select to filter GET
    +Made userName field Unique

--- orchestra-manager-backend/routes/users.js
+++ orchestra-manager-backend/routes/users.js
@@ -3,13 +3,15 @@ let User = require ('../models/users.model');
 
 router.route('/').get((req, res) => {
     User.find()
+        .select('_id userType fName lName password email userName')
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err))
 });
 
 router.route('/:id').get((req, res) => {
     User.findById(req.params.id)
-        .then(user => res.json(excercise))
+        .select('_id userType fName lName password email userName')
+        .then(user => res.json(user))
         .catch(err => res.status(400).json('Error' + err))
 });
 
