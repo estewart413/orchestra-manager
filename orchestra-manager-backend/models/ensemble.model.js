c3345797a9783c41e3b540ad2966f62fb75f7f64
estewart413 <dane.stewart@selu.edu>
2020-05-02 14:41:43 -0500

    Added user role assignment routing/schema changes
    Updated the ensemble schema to account for multiple users in each ensemble area as well as to keep updated with current frontend work.  Added routing for assignment of user roles.

--- orchestra-manager-backend/models/ensemble.model.js
+++ orchestra-manager-backend/models/ensemble.model.js
@@ -6,6 +6,7 @@ var ensembleSchema = new Schema({
     enId:    {type: String, required: true, unique: true},        //Ensemble Unique Id
     enName:       {type: String, required: true},   //Ensemble Unique Name
     enType:       {type: String, required: true},   //Ensemble Type , Ex. Jazz Band, Symphony Orchestra
+    chairs:       { type: [] },
     accManager:    {type: [String]},                  //User Type Manager
     accConduct:       {type: [String]},               //User Type Conductor
     accLibrarian:    {type: [String]},                //User Type Librarian
--- orchestra-manager-backend/routes/ensemble.js
+++ orchestra-manager-backend/routes/ensemble.js
@@ -17,6 +17,7 @@ router.route('/add').post((req, res) => {
     const enId = req.body.enId;
     const enName = req.body.enName;
     const enType = req.body.enType;
+    const chairs = req.body.chairs;
     const accManager = req.body.accManager;
     const accConduct = req.body.accConduct;
     const accLibrarian = req.body.accLibrarian;
@@ -26,6 +27,7 @@ router.route('/add').post((req, res) => {
         enId,
         enName,
         enType,
+        chairs,
         accManager,
         accConduct ,
         accLibrarian,
@@ -52,8 +54,10 @@ router.route('/edit/:id').put((req, res) => {
             ensemble.enName = req.body.enName;
             if (ensemble.enType != req.body.enType)
             ensemble.enType = req.body.enType;
+            if (ensemble.chairs != req.body.chairs)
+            ensemble.chairs = req.body.chairs;
             if (ensemble.accManager != req.body.accManager)
-            ensemble.password = req.body.password;
+            ensemble.accManager = req.body.accManager;
             if (ensemble.accConduct != req.body.accConduct)
             ensemble.email = req.body.email;
             if (ensemble.accLibrarian != req.body.accLibrarian)
@@ -63,31 +67,95 @@ router.route('/edit/:id').put((req, res) => {
         });
 });
 
-router.route('/addMember/').put(async (req, res) => {
+router.route('/addMember/').post(async (req, res) => {
     let userName = req.body.userName;
-    let enID = req.body.enID;
+    let enId = req.body.enId;
 
     try {
-        let query = await Ensemble.findOne({"enId": enID },{_id:0, enID:0, enName:0, enType:0, accManager:0, accConduct:0, accLibrarian:0, __v:0})
+        let query = await Ensemble.findOne({"enId":enId})
         .catch(err => console.log(err).json("Error:" + err));
-        console.log(query.accMember);
+        console.log(query);
         let memberList = await query.accMember;
         let addedCheck = "";
         await memberList.forEach(element => {
             if (element==userName) {
                 addedCheck = true;
-                console.log(userName).json("Username already added.");
+                console.log(userName);
             }
-            });
+        });
         if (addedCheck != true) {
             memberList.push(userName);
             query.accMember = memberList;
             query.save();
+            res.status(200).json("Member Added!")
+        }
+        else {
+            res.status(400).json("Username already exists!")
         }
     }
     catch (err) {
         console.log(err).json("Error" + err);
     }
-    });
+});
 
+router.route('/addLibrarian/').post(async (req, res) => {
+    let userName = req.body.userName;
+    let enId = req.body.enId;
+
+    try {
+        let query = await Ensemble.findOne({"enId":enId})
+        .catch(err => console.log(err).json("Error:" + err));
+        console.log(query);
+        let libList = await query.accLibrarian;
+        let addedCheck = "";
+        await libList.forEach(element => {
+            if (element==userName) {
+                addedCheck = true;
+                console.log(userName);
+            }
+        });
+        if (addedCheck != true) {
+            libList.push(userName);
+            query.accLibrarian = memberList;
+            query.save();
+            res.status(200).json("Librarian Added!")
+        }
+        else {
+            res.status(400).json("Username already exists!")
+        }
+    }
+    catch (err) {
+        console.log(err).json("Error" + err);
+    }
+});
+router.route('/addConductor/').post(async (req, res) => {
+    let userName = req.body.userName;
+    let enId = req.body.enId;
+
+    try {
+        let query = await Ensemble.findOne({"enId":enId})
+        .catch(err => console.log(err).json("Error:" + err));
+        console.log(query);
+        let conList = await query.accConductor;
+        let addedCheck = "";
+        await conList.forEach(element => {
+            if (element==userName) {
+                addedCheck = true;
+                console.log(userName);
+            }
+        });
+        if (addedCheck != true) {
+            conList.push(userName);
+            query.accConductor = memberList;
+            query.save();
+            res.status(200).json("Librarian Added!")
+        }
+        else {
+            res.status(400).json("Username already exists!")
+        }
+    }
+    catch (err) {
+        console.log(err).json("Error" + err);
+    }
+});
 module.exports = router;
