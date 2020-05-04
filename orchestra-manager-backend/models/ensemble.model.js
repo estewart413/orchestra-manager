b0b8fc9c0879099163bacd44d5028d7378fd0e13
estewart413 <dane.stewart@selu.edu>
2020-05-03 19:30:27 -0500

    Update Ensemble Schema
    Removed required:true from enType

--- orchestra-manager-backend/models/ensemble.model.js
+++ orchestra-manager-backend/models/ensemble.model.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var ensembleSchema = new Schema({
     enId:    {type: String, required: true, unique: true},        //Ensemble Unique Id
     enName:       {type: String, required: true},   //Ensemble Unique Name
-    enType:       {type: String, required: true},   //Ensemble Type , Ex. Jazz Band, Symphony Orchestra
+    enType:       {type: String},   //Ensemble Type , Ex. Jazz Band, Symphony Orchestra
     chairs:       { type: [] },
     accManager:    {type: [String]},                  //User Type Manager
     accConduct:       {type: [String]},               //User Type Conductor
